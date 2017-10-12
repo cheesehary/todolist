620682c8fa1d6200bbe306ba85f899e34285f735
王禹衡 <yuheng.wang@froginfo.cn>
2017-10-12 23:25:02 +0800

    init basics

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,12 @@
 import { ADD_TODO, CHECK_TODO, DELETE_TODO } from './types';
 
+let index = 1;
+export const addTodo = label => ({
+  type: ADD_TODO,
+  id: ++index,
+  label
+})
+
 export const checkTodo = id => ({
   type: CHECK_TODO,
   id
